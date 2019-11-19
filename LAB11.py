ac50697e3a4c2974a5256ae04266fc084f81e128
jyyblue <jyyblue1987@outlook.com>
2019-11-19 10:05:30 +0200

    [add] # save a dictionary with the state of the list after each complete pass

--- LAB11.py
+++ LAB11.py
@@ -19,18 +19,21 @@ def selectionSort(numList):
     '''
     # YOUR CODE STARTS HERE
     # Traverse through all array elements 
+    state = {}
     for i in range(len(numList)): 
-        
+        # save a dictionary with the state of the list after each complete pass        
+        state[i + 1] = numList.copy()
+                
         # Find the minimum element in remaining  
         # unsorted array 
         min_idx = i 
         for j in range(i+1, len(numList)): 
             if numList[min_idx] > numList[j]: 
                 min_idx = j 
-                
+        
         # Swap the found minimum element with  
         # the first element         
         numList[i], numList[min_idx] = numList[min_idx], numList[i] 
     
     # Driver code to test above 
-    return numList
+    return state, numList
