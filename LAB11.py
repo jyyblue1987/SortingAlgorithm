4a3b0a57b1b5527c0e13be40d3f4b34dd4a604f6
jyyblue <jyyblue1987@outlook.com>
2019-11-19 09:50:24 +0200

    [add] create project.

---
+++ LAB11.py
@@ -0,0 +1,20 @@
+#LAB 11
+#Due Date: 11/22/2019, 11:59PM
+########################################
+#                                      
+# Name:
+# Collaboration Statement:             
+#
+########################################
+
+
+def selectionSort(numList):
+    '''
+        Takes a list and returns 2 values
+        1st returned value: a dictionary with the state of the list after each complete pass of selection sort
+        2nd returned value: the sorted list
+
+        >>> selectionSort([9,3,5,4,1,78,67])
+        ({1: [9, 3, 5, 4, 1, 78, 67], 2: [1, 3, 5, 4, 9, 78, 67], 3: [1, 3, 5, 4, 9, 78, 67], 4: [1, 3, 4, 5, 9, 78, 67], 5: [1, 3, 4, 5, 9, 78, 67], 6: [1, 3, 4, 5, 9, 78, 67], 7: [1, 3, 4, 5, 9, 67, 78]}, [1, 3, 4, 5, 9, 67, 78])
+    '''
+    # YOUR CODE STARTS HERE
---
+++ LAB12.py
@@ -0,0 +1,21 @@
+#LAB 12
+#Due Date: 11/22/2019, 11:59PM
+########################################
+#                                      
+# Name:
+# Collaboration Statement:             
+#
+########################################
+
+class MinHeap:
+    ### Copy and paste your code from LAB 9 here
+
+
+
+def heapSort(numList):
+    '''
+       >>> heapSort([9,7,4,1,2,4,8,7,0,-1])
+       [-1, 0, 1, 2, 4, 4, 7, 7, 8, 9]
+    '''
+    sort_heap = MinHeap()
+    # -- YOUR CODE STARTS HERE
